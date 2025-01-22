31b86babd34f9d6ddb508c3cdc90aa40ee1b3e5b
casey829 <caseymwangi31@gmail.com>
2025-01-22 14:25:02 +0300

    completed the ui of signing in and adding link to sign up

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -1,40 +1,3 @@
-{/*import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
-import React, { useState } from 'react'
-import icons from '../constants/icons'
-
-
-const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ...props}) => {
-    const [showPassword, setShowPassword] = useState(false)
-    const [isFocused, setIsFocused] = useState(false)
-
-    return (
-        <View className={`space-y-2 ${otherStyles}`}>
-            <Text className='text-base text-gray-100 font-pmedium'>{title}</Text>
-            <TextInput 
-                className={`w-full h-16 px-4 bg-black-100 border-2 rounded-2xl text-white font-psemibold text-base
-                    ${isFocused ? 'border-orange-500' : 'border-black-200'}`}
-                value={value}
-                placeholder={placeholder}
-                placeholderTextColor='#7b7b8b'
-                onChangeText={handleChangeText}
-                secureTextEntry={title === 'Password' && !showPassword}
-                onFocus={() => setIsFocused(true)}
-                onBlur={() => setIsFocused(false)}
-            />
-            {title === 'Password' && (
-                <TouchableOpacity onPress={() =>
-                    setShowPassword(!showPassword)}>
-                    <Image 
-                      source={!showPassword ? icons.eye : icons.eyehide}
-                    
-                    />
-                </TouchableOpacity>
-            )}
-        </View>
-    )
-}
-
-export default FormField*/}
 import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
 import React, { useState } from 'react'
 import icons from '../constants/icons'
@@ -65,7 +28,7 @@ const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ..
                     >
                         <Image 
                             source={!showPassword ? icons.eye : icons.eyehide}
-                            className="w-6 h-6"  // Adjust size as needed
+                            className="w-6 h-6"
                         />
                     </TouchableOpacity>
                 )}
